a871328a96a8f4c0c7e8b7afb67806fbf11ba801
RiccardoCasula <47038148+Swano4907@users.noreply.github.com>
2021-08-11 17:15:04 +0200

    Aggiunta test unitari per il modello account compresi di test per super user.

--- progISW/account/models.py
+++ progISW/account/models.py
@@ -6,58 +6,67 @@ from struttureGioco.models import Personaggio
 # Create your models here.
 
 class MyAccountManager(BaseUserManager):
-    def create_user(self, playerID, password=None):
-        if not playerID:
-            raise ValueError("L'utente deve avere uno username")
-        user = self.model(
-            playerID=playerID
-        )
+	def create_user(self, playerID, password=None):
+		# eccezione in caso in cui playerID sia vuoto
+		if not playerID:
+			raise ValueError("L'utente deve avere uno username")
 
-        user.set_password(password)
-        user.save(using=self._db)
+		# eccezione in caso in cui password sia vuota
+		if not password:
+			raise ValueError("La password non può essere vuota")
+		user = self.model(
+			playerID=playerID
+		)
 
-        return user
+		user.set_password(password)
+		user.save(using=self._db)
 
-    def create_superuser(self, playerID, password):
+		return user
 
-        if not playerID:
-            raise ValueError("L'utente deve avere uno username")
-        user = self.create_user(
-            playerID=playerID,
-            password=password,
+	def create_superuser(self, playerID, password):
 
-        )
-        user.is_admin = True
-        user.is_staff = True
-        user.is_superuser = True
-        user.save(using=self._db)
-        return user
+		if not playerID:
+			raise ValueError("L'utente deve avere uno username")
+
+			# eccezione in caso in cui password sia vuota
+		if not password:
+			raise ValueError("La password non può essere vuota")
+
+		user = self.create_user(
+			playerID=playerID,
+			password=password,
+
+		)
+		user.is_admin = True
+		user.is_staff = True
+		user.is_superuser = True
+		user.save(using=self._db)
+		return user
 
 
 class Account(AbstractBaseUser):
-    playerID = models.CharField(max_length=50, unique=True)
-    date_joined = models.DateTimeField(verbose_name='date joined', auto_now_add=True)
-    last_login = models.DateTimeField(verbose_name='last login', auto_now=True)
-    is_admin = models.BooleanField(default=False)
-    is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=False)
-    is_superuser = models.BooleanField(default=False)
-    personaggio = models.ForeignKey(Personaggio, on_delete=models.CASCADE, null=True)
+	playerID = models.CharField(max_length=50, unique=True)
+	date_joined = models.DateTimeField(verbose_name='date joined', auto_now_add=True)
+	last_login = models.DateTimeField(verbose_name='last login', auto_now=True)
+	is_admin = models.BooleanField(default=False)
+	is_active = models.BooleanField(default=True)
+	is_staff = models.BooleanField(default=False)
+	is_superuser = models.BooleanField(default=False)
+	personaggio = models.ForeignKey(Personaggio, on_delete=models.CASCADE, null=True)
 
-    # indico con che campo farò il login
-    USERNAME_FIELD = "playerID"
+	# indico con che campo farò il login
+	USERNAME_FIELD = "playerID"
 
-    # campi obbligatori
+	# campi obbligatori
 
-    objects = MyAccountManager()
+	objects = MyAccountManager()
 
-    def __str__(self):
-        return self.playerID
+	def __str__(self):
+		return self.playerID
 
-    def has_perm(self, perm, obj=None):
-        return self.is_admin
+	def has_perm(self, perm, obj=None):
+		return self.is_admin
 
-    # Does this user have permission to view this app? (ALWAYS YES FOR SIMPLICITY)
-    def has_module_perms(self, app_label):
-        return True
-
+	# Does this user have permission to view this app? (ALWAYS YES FOR SIMPLICITY)
+	def has_module_perms(self, app_label):
+		return True
--- progISW/account/tests.py
+++ progISW/account/tests.py
@@ -1,3 +1,43 @@
-from django.test import TestCase
+# from django.core.exceptions import ValidationError
+from django.test import TestCase, Client
+# from django.contrib.auth import login, authenticate, logout
+from account.models import Account
+import unittest
+
 
 # Create your tests here.
+# Tutte le funzioni di test devono iniziare con test_<qualcosa>
+class AccountTest(TestCase):
+	def setUp(self):
+		self.account = Account.objects.create_user(playerID='Riccardo', password='Fallito234')
+		self.superAccount = Account.objects.create_superuser(playerID="Asu", password="Fantastico123")
+
+	# creazione utente di prova per il test unitario
+	def test_expected_str_return_value(self):
+		self.assertEqual(self.account.__str__(), "Riccardo")
+
+	def test_verifica_username_registrazione(self):
+		self.assertRaises(ValueError, Account.objects.create_user, playerID='', password='prova123')
+
+	def test_verifica_password_registrazione(self):
+		self.assertRaises(ValueError, Account.objects.create_user, playerID='dwdw', password='')
+
+	def test_superUser_username_registrazione(self):
+		self.assertRaises(ValueError, Account.objects.create_superuser, playerID='', password='ddddd')
+
+	def test_superUser_password_registrazione(self):
+		self.assertRaises(ValueError, Account.objects.create_superuser, playerID='dwdw', password='')
+
+	def test_superUser_isAdmin(self):
+		self.assertTrue(self.superAccount.is_admin)
+
+	# self.assertTrue(self.account.is_admin) fallisce sempre
+	def test_superUser_isStaff(self):
+		self.assertTrue(self.superAccount.is_staff)
+
+	def test_superUser_isSuperUser(self):
+		self.assertTrue(self.superAccount.is_superuser)
+
+
+if __name__ == "__main__":
+	unittest.main()
