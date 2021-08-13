521c62b3d7916b5571c6a5d837a5d950c852bab7
RiccardoCasula <47038148+Swano4907@users.noreply.github.com>
2021-08-13 19:03:59 +0200

    Implementazione cambio equipaggiamento

--- progISW/home/views.py
+++ progISW/home/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 
-
 # Create your views here.
+from struttureGioco.models import Equipaggiamento
+
 
 def homeView(request):
     user = request.user
@@ -12,8 +13,23 @@ def homeView(request):
 
 def inventarioView(request):
     user = request.user
+    if request.POST:
+        itemID = request.POST.get("idItem")
+        item = Equipaggiamento.objects.get(pk=itemID)
+        if item.tipo == "P":
+            user.personaggio.armaPrimaria = item
+            # user.personaggio.save()
+        elif item.tipo == "S":
+            user.personaggio.armaSecondaria = item
+            # user.personaggio.save()
+        else:
+            user.personaggio.armatura = item
+
+        user.personaggio.save()
+
     if not user.is_authenticated:
         return redirect("login")
+
     zaino = user.personaggio.zaino.all()
 
     return render(request, "inventario.html", {'zaino': zaino})
--- progISW/struttureGioco/models.py
+++ progISW/struttureGioco/models.py
@@ -1,71 +1,73 @@
 from django.db import models
 
 # Create your models here.
+from django.urls import reverse
+
 TIPOEQUIPAGGIAMENTO = [
 
-	("P", "ArmaPrimaria"),
-	("S", "ArmaSecondaria"),
-	("A", "Armatura"),
+    ("P", "ArmaPrimaria"),
+    ("S", "ArmaSecondaria"),
+    ("A", "Armatura"),
 ]
 
 
 class Boss(models.Model):
-	nome = models.CharField(max_length=50, unique=True)
-	luogo = models.CharField(max_length=100, null=True)
-	vita = models.IntegerField(default=10)
-	abilitato = models.BooleanField(default=False)
+    nome = models.CharField(max_length=50, unique=True)
+    luogo = models.CharField(max_length=100, null=True)
+    vita = models.IntegerField(default=10)
+    abilitato = models.BooleanField(default=False)
 
-	vitalita = models.IntegerField(default=0)
-	forza = models.IntegerField(default=0)
-	destrezza = models.IntegerField(default=0)
-	intelligenza = models.IntegerField(default=0)
-	tempra = models.IntegerField(default=0)
+    vitalita = models.IntegerField(default=0)
+    forza = models.IntegerField(default=0)
+    destrezza = models.IntegerField(default=0)
+    intelligenza = models.IntegerField(default=0)
+    tempra = models.IntegerField(default=0)
 
-	def __str__(self):
-		return self.nome
+    def __str__(self):
+        return self.nome
 
 
 class Equipaggiamento(models.Model):
-	nome = models.CharField(max_length=50, unique=True)
-	tipo = models.CharField(max_length=1, choices=TIPOEQUIPAGGIAMENTO, null=True)
-	abilitato = models.BooleanField(default=True)
-	boss = models.ForeignKey("Boss", on_delete=models.CASCADE, blank=True, null=True)
+    nome = models.CharField(max_length=50, unique=True)
+    tipo = models.CharField(max_length=1, choices=TIPOEQUIPAGGIAMENTO, null=True)
+    abilitato = models.BooleanField(default=True)
+    boss = models.ForeignKey("Boss", on_delete=models.CASCADE, blank=True, null=True)
 
-	vitalita = models.IntegerField(default=0)
-	forza = models.IntegerField(default=0)
-	destrezza = models.IntegerField(default=0)
-	intelligenza = models.IntegerField(default=0)
-	tempra = models.IntegerField(default=0)
+    vitalita = models.IntegerField(default=0)
+    forza = models.IntegerField(default=0)
+    destrezza = models.IntegerField(default=0)
+    intelligenza = models.IntegerField(default=0)
+    tempra = models.IntegerField(default=0)
 
-	def __str__(self):
-		return self.nome
+    def __str__(self):
+        return self.nome
 
 
 class Personaggio(models.Model):
-	nome = models.CharField(max_length=50, null=True)
-	armaPrimaria = models.ForeignKey(Equipaggiamento, null=True, on_delete=models.CASCADE, blank=True, related_name='+')
-	armaSecondaria = models.ForeignKey(Equipaggiamento, null=True, on_delete=models.CASCADE, blank=True,
-									   related_name='+')
-	armatura = models.ForeignKey(Equipaggiamento, null=True, on_delete=models.CASCADE, blank=True, related_name='+')
+    nome = models.CharField(max_length=50, null=True)
+    armaPrimaria = models.ForeignKey(Equipaggiamento, null=True, on_delete=models.CASCADE, blank=True, related_name='+')
+    armaSecondaria = models.ForeignKey(Equipaggiamento, null=True, on_delete=models.CASCADE, blank=True,
+                                       related_name='+')
+    armatura = models.ForeignKey(Equipaggiamento, null=True, on_delete=models.CASCADE, blank=True, related_name='+')
 
-	vita = models.IntegerField(default=10)
-	vitalita = models.IntegerField(default=0)
-	forza = models.IntegerField(default=0)
-	destrezza = models.IntegerField(default=0)
-	intelligenza = models.IntegerField(default=0)
-	tempra = models.IntegerField(default=0)
-	zaino = models.ManyToManyField(Equipaggiamento, through='Inventario')
+    vita = models.IntegerField(default=10)
+    vitalita = models.IntegerField(default=0)
+    forza = models.IntegerField(default=0)
+    destrezza = models.IntegerField(default=0)
+    intelligenza = models.IntegerField(default=0)
+    tempra = models.IntegerField(default=0)
+    zaino = models.ManyToManyField(Equipaggiamento, through='Inventario')
 
-	def __str__(self):
-		return self.nome
+    def __str__(self):
+        return self.nome
 
 
 class Inventario(models.Model):
-	personaggio = models.ForeignKey(Personaggio, null=False, on_delete=models.CASCADE)
-	equipaggiamento = models.ForeignKey(Equipaggiamento, null=False, on_delete=models.CASCADE)
+    personaggio = models.ForeignKey(Personaggio, null=False, on_delete=models.CASCADE)
+    equipaggiamento = models.ForeignKey(Equipaggiamento, null=False, on_delete=models.CASCADE)
 
-	def __str__(self):
-		return self.personaggio.__str__() + ' --> ' + self.equipaggiamento.__str__()
+    def __str__(self):
+        return self.personaggio.__str__() + ' --> ' + self.equipaggiamento.__str__()
 
-	class Meta:
-		unique_together = ('personaggio', 'equipaggiamento')
+    class Meta:
+        unique_together = ('personaggio', 'equipaggiamento')
